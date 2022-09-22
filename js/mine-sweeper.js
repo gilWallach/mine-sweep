f030505f709129575d30ca36fc403e0217176805
gilWallach <104685476+gilWallach@users.noreply.github.com>
2022-09-22 20:30:13 +0300

    third upload

--- js/mine-sweeper.js
+++ js/mine-sweeper.js
@@ -1,16 +1,16 @@
 'use strict'
-const MINE = '💣'
+const MINE = '💥'
 const FLAG = '🚩'
 
-var gSmily = '🙂'
-var gTimer = document.querySelector('.timer span')
+var gSmily
 var gtimerInterval
-
 
 var gLevel = {
     SIZE: 4,
     MINES: 2
 }
+console.log(gLevel)
+
 
 var gGame = {
     isOn: false,
@@ -20,32 +20,38 @@ var gGame = {
     gameLost: false
 }
 
-var gBoard = createBoard(gLevel.SIZE)
+var gBoard
 
 function init() {
-    resetgGame()
-    createBoard(gLevel.SIZE)
+    gSmily = '🙂'
+    gBoard = createBoard(gLevel.SIZE)
     setMinesNegsCount(gBoard)
     renderBoard(gBoard)
-
 }
-
-// console.log(gBoard)
 
 function createBoard(size) {
     var mat = []
-    // TODO: get rndIdx for mine cells
+
     for (var i = 0; i < size; i++) {
         mat[i] = []
         for (var j = 0; j < size; j++) {
+            var isMine = false
+            mat[i].push(createCell(isMine))
 
-            if ((i === 2 && j === 3) || (i === 3 && j === 1)) {
+            if ((i === 3 && j === 2) || (i === 1 && j === 0)) {
                 isMine = true
-                mat[i].push(createCell(isMine))
-            } else {
-                var isMine = false
-                mat[i].push(createCell(isMine))
+                mat[i][j] = createCell(isMine)
             }
+
+            // TODO: fix rndIdx for mine cells + adapt for levels
+            // for(var k = 0; k <gLevel.MINES; k++){
+            //     var rndI = getRandomIntInclusive(0, size - 1)
+            //     var rndJ = getRandomIntInclusive(0, size - 1)
+            //     if(i === rndI && j === rndJ){
+            //         isMine = true
+            //         mat[i][j] = createCell(isMine)
+            //     }
+            // }
 
         }
     }
@@ -83,57 +89,55 @@ function renderBoard(board) {
     elBoard.innerHTML = strHTML
 }
 
-
 function cellClicked(elCell, i, j) {
-    gGame.isOn = true
-    
+    // start timer
+    if (!gGame.isOn) {
+        gGame.isOn = true
+        showTimer()
+    }
+    // if cell is already clicked
     if (gBoard[i][j].isShown) {
         gBoard[i][j].isShown = false
         gGame.shownCount--
-
-        elCell.innerText = ``
-        elCell.classList.remove('shown')
     }
-    else { 
-        if (gBoard[i][j].isMine === false) {
-            // TODO: switch cases?
+    // if cell isn't clicked yet
+    else {
+        // TODO: switch cases maybe more clear
+        if (!gBoard[i][j].isMine) {
             if (gBoard[i][j].minesAroundCount !== 0) {
                 // Update the model
                 gBoard[i][j].isShown = true
                 gGame.shownCount++
-    
+
                 checkGameOver()
-    
+
                 // Update the DOM
                 elCell.innerText = `${gBoard[i][j].minesAroundCount}`
             } else {
                 // Update the model
                 gBoard[i][j].isShown = true
                 gGame.shownCount++
-                revealNegs(gBoard, i, j)
+                expandShown(gBoard, i, j)
                 checkGameOver()
-    
+
             }
             // Update the DOM
             elCell.classList.add('shown')
         }
-    
         else {
             // Update the model
             gGame.gameLost = true
             checkGameOver()
-    
+
             // Update the DOM
-    
             elCell.innerText = `${MINE}`
         }
-
-     }
+    }
 }
 
-
 function cellMarked(elCell, i, j) {
-    if (gBoard[i][j].isMarked === false) {
+    // if cell isn't marked yet
+    if (!gBoard[i][j].isMarked) {
         // Update the model
         gBoard[i][j].isMarked = true
         gGame.markedCount++
@@ -144,7 +148,9 @@ function cellMarked(elCell, i, j) {
             elCell.innerText = `${FLAG}`
             e.preventDefault();
         }, false)
-    } else {
+    } 
+    // if cell is already marked
+    else {
         // Update the model
         gGame.markedCount--
         gBoard[i][j].isMarked = false
@@ -160,23 +166,43 @@ function cellMarked(elCell, i, j) {
 function checkGameOver() {
     var msg
     var elMsg = document.querySelector('.msg')
-    console.log('gGame.markedCount', gGame.markedCount)
-    console.log('gGame.shownCount', gGame.shownCount)
+    // Win
     if (gGame.markedCount === gLevel.MINES &&
         gGame.shownCount === gLevel.SIZE ** 2 - gGame.markedCount) {
+        // Update the model
+
+        gGame.isOn = false
+        clearInterval(gtimerInterval)
+        
+        // Update the DOM
         msg = 'You Win!'
         gSmily = '😎'
         elMsg.innerText = msg
         document.querySelector('.smily').innerText = gSmily
+        // elMsg.classList.add('msg-shown')
     }
-    else if (gGame.gameLost === true) {
+    // Lose
+    else if (gGame.gameLost) {
         // Update the model
         gGame.isOn = false
-        msg = 'You Lose'
+        clearInterval(gtimerInterval)
+        
+        // Update the DOM
+        msg = 'You Lose...'
         gSmily = '🤯'
         elMsg.innerText = msg
+        
         document.querySelector('.smily').innerText = gSmily
+        // elMsg.classList.add('msg-shown')
     }
+}
+
+function restartGame() {
+    clearInterval(gtimerInterval)
+    resetgGame()
+    init()
+
+    document.querySelector('.msg').innerText = ''
 }
 
 function showTimer() {
@@ -190,12 +216,44 @@ function showTimer() {
             var secs = parseInt((currTs - start) / 1000)
             var ms = (currTs - start) - secs * 1000
             ms = '000' + ms
-            // 00034 // 0001
             ms = ms.substring(ms.length - 3, ms.length)
 
             timer.innerText = `\n ${secs}:${ms}`
         }, 100)
-
     }
+}
 
+function expandShown(board, i, j) {
+    var rowIdx = i
+    var colIdx = j
+
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            var negs = []
+
+            if (j < 0 || j >= board[i].length) continue
+            if (i === rowIdx && j === colIdx) continue
+
+            if (!gBoard[i][j].isShown) {
+                //update the model
+                gBoard[i][j].isShown = true
+                negs.push(gBoard[i][j])
+                gGame.shownCount += negs.length
+            }
+
+
+            // update the DOM
+            var selector = `[data-i="${i}"][data-j="${j}"]`
+            var elneg = document.querySelector(selector)
+
+            if (gBoard[i][j].minesAroundCount !== 0) {
+                elneg.innerText = `${gBoard[i][j].minesAroundCount}`
+            }
+            if (gBoard[i][j].isShown) {
+                elneg.classList.add('shown')
+            }
+        }
+    }
 }
--- js/util.js
+++ js/util.js
@@ -27,40 +27,25 @@ function countMinesAround(board, rowIdx, colIdx) {
     return minesCount
 }
 
-function revealNegs(board, rowIdx, colIdx) {
-    var negs = []
-    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
-        if (i < 0 || i >= board.length) continue
-
-        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
-
-            if (j < 0 || j >= board[i].length) continue
-            if (i === rowIdx && j === colIdx) continue
-            // update the model
-            if (gBoard[i][j].isShown === false) {
-                gBoard[i][j].isShown = true
-                negs.push(board[i][j])
-            }
-            // update the DOM
-            var selector = `[data-i="${i}"][data-j="${j}"]`
-            var elCell = document.querySelector(selector)
-
-            if (gBoard[i][j].minesAroundCount !== 0) {
-                elCell.innerText = `${gBoard[i][j].minesAroundCount}`
-            }
-            if (gBoard[i][j].isShown) {
-                elCell.classList.add('shown')
-            }
-        }
-    }
-    gGame.shownCount += negs.length
+function setLevel(size, amountOfMines) {
+    gLevel.SIZE = size
+    gLevel.MINES = amountOfMines
+    console.log(gLevel)
+    init()
 }
+
 
 function resetgGame() {
     return gGame = {
         isOn: false,
         shownCount: 0,
         markedCount: 0,
-        secsPassed: 0
+        secsPassed: 0,
+        gameLost: false
+
     }
+}
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
